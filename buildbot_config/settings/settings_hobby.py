7c2e7f79e1451357f45731bdd58d858673b76ff3
cartoonmed <attakornpks@proteus-tech.com>
2013-08-21 12:46:33 +0700

    change repos dir for buildslave

--- buildbot_config/settings/settings_hobby.py
+++ buildbot_config/settings/settings_hobby.py
@@ -1,5 +1,5 @@
 BRANCH = "develop"
 MASTER_HOST = "ec2-54-254-127-209.ap-southeast-1.compute.amazonaws.com"
-PROJECT_CODE_URL = "hobby"
+PROJECT_CODE_URL = "src"
 PROJECT_NAME = "hobby"
 REPOSITORY_URL = "https://github.com/AttakornP/hobby.git"
