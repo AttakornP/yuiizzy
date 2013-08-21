65833e6377463ebb45970f91a2f3a62733d55ec7
cartoonmed <attakornpks@proteus-tech.com>
2013-08-21 12:41:58 +0700

    change buildbot config

--- buildbot_config/settings/settings_hobby.py
+++ buildbot_config/settings/settings_hobby.py
@@ -3,4 +3,3 @@ MASTER_HOST = "ec2-54-254-127-209.ap-southeast-1.compute.amazonaws.com"
 PROJECT_CODE_URL = "hobby"
 PROJECT_NAME = "hobby"
 REPOSITORY_URL = "https://github.com/AttakornP/hobby.git"
-WORKDIR = '/home/www-data/Buildbot/src/'
